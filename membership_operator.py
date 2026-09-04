7288b206100ddc4d296466df8fa502341faaaafb
saipalli2008 <pallisai2008@gmail.com>
2026-09-04 14:21:53 +0530

    membership operator

---
+++ membership_operator.py
@@ -0,0 +1,33 @@
+#in membership operator
+a = [1, 2, 3, 4, 5]
+b= 3
+result = b in a
+print("result of",b,"in",a,"is:",result)
+#not in membership operator
+a=[7,8,9,10]
+b= 5
+result = b not in a
+print("result of",b,"not in",a,"is:",result)
+#in membership operator
+a=[5,6,7,8]
+b= 9
+result = b in a
+print("result of",b,"in",a,"is:",result)
+
+#not in membership operator
+a=[1,2,3,4]
+b= 6
+result = b not in a
+print("result of",b,"not in",a,"is:",result)
+
+#in membership operator
+a=[3,4,5,6]
+b= 9
+result = b in a
+print("result of",b,"in",a,"is:",result)
+
+#not in membership operator
+a=[4,6,8,10]
+b=4
+result = b not in a
+print("result of",b,"not in",a,"is:",result)

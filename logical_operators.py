c4f786be512b0181ec81c1aa9d407828b528dba6
saipalli2008 <pallisai2008@gmail.com>
2026-09-07 18:26:46 +0530

    logical operators

---
+++ logical_operators.py
@@ -0,0 +1,21 @@
+a=45 #and opertor
+result =a>15 and a<50
+print("result of",a,">15 and <",a,"<50 is:",result)
+b=30
+result =b>10 and b<20
+print("result of",b,">10 and <",b,"<20 is:",result)
+#or operator
+h=6
+result=h>5 or h<10
+print("result of",h,">5 or <",h,"<10 is:",result)
+n=12
+result=n>15 or n<20
+print("result of",n,">15 or <",n,"<20 is:",result)
+
+#not operator
+x=10
+result=not(x>5)
+print("result of not(",x,">5) is:",result)
+f=15
+result=not(f<20)
+print("result of not(",f,"<20) is:",result)

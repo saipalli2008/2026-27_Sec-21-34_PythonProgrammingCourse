d24bf7407186beabb031be4fea386b487b304000
saipalli2008 <pallisai2008@gmail.com>
2026-09-08 13:33:50 +0530

    comparision operator

---
+++ comparision_operators_excercise.py
@@ -0,0 +1,20 @@
+#python comparision operators
+a=29
+b=14
+result = a==b # equal to comparision
+print("result of",a, "==",b,"is : ",result)
+
+result = a!=b #not equalto comparison
+print("result of",a, "!=",b,"is : ",result)
+
+result = a > b #greaterthan comparison
+print("result of",a, ">",b,"is : ",result)
+
+result = a < b #lessthan comparision
+print("result of",a, "<",b,"is : ",result)
+
+result = a >= b #greaterthan or equalto comparison
+print("result of",a, ">=",b,"is : ",result)
+
+result = a <= b #lessthan or equalto comparision
+print("result of",a, "<=",b,"is : ",result)

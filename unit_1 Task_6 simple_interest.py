96ac143d3ba5938dec338e07886a76709d488754
saipalli2008 <pallisai2008@gmail.com>
2026-09-09 11:27:12 +0530

    simple interest

---
+++ unit_1 Task_6 simple_interest.py
@@ -0,0 +1,5 @@
+p=int(input("Enter the principal amount: "))
+r=int(input("Enter the rate of interest: "))
+t=int(input("Enter the time period: "))
+si=(p*r*t)/100
+print("The simple interest is:",si)

7ab595dbd8beed3b48d320ea0f37d5d5db6b47b2
saipalli2008 <pallisai2008@gmail.com>
2026-09-25 14:56:46 +0530

    anumber is positive-negative-zero

---
+++ unit_2 Task-3 check_a number is positive-negative-zero.py
@@ -0,0 +1,8 @@
+# input for checking a number is positive, negative ,zero.and
+number= float(input("Enter a number: "))
+if number > 0:
+    print("The number  is positive")
+elif number < 0:
+    print("The number  is negative")
+else:
+    print("The number is zero ") 

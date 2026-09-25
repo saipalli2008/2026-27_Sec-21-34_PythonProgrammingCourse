282a54409649c12c25099f9252dc77fe184a7ebb
saipalli2008 <pallisai2008@gmail.com>
2026-09-25 14:42:48 +0530

    Eligibility for vote

---
+++ unit_2 task-2 check_eligibility_for_vote.py
@@ -0,0 +1,7 @@
+# Eligibility for vote
+#input for age.
+age_in_years=int(input("Enter your age"))
+if age_in_years >= 18:
+    print(f "Eligible to vote")
+else:
+    print(f "Not eligible to vote")
